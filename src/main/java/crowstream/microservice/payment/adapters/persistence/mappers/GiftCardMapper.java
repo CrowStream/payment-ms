ebba9a89711e2098fb2f96a71a10d94933ec7938
Jorge Aurelio Morales Manrique <jomorales@unal.edu.co>
2021-11-27 16:50:17 -0500

    Update gift card mapper

--- src/main/java/crowstream/microservice/payment/adapters/persistence/mappers/GiftCardMapper.java
+++ src/main/java/crowstream/microservice/payment/adapters/persistence/mappers/GiftCardMapper.java
@@ -6,13 +6,20 @@ import crowstream.microservice.payment.application.domain.GiftCard;
 public class GiftCardMapper {
     public GiftCardEntity pojoToEntity(GiftCard pojo) {
         GiftCardEntity entity = new GiftCardEntity();
-
+        entity.setCardCode(pojo.getCardCode());
+        entity.setAmount(pojo.getAmount());
+        entity.setActive(pojo.getActive());
+        entity.setExpirationDate(pojo.getExpirationDate());
         return entity;
     }
 
     public GiftCard entityToPojo(GiftCardEntity entity) {
         GiftCard pojo = new GiftCard();
-
+        pojo.setId(entity.getId());
+        pojo.setCardCode(entity.getCardCode());
+        pojo.setAmount(entity.getAmount());
+        pojo.setActive(entity.getActive());
+        pojo.setExpirationDate(entity.getExpirationDate());
         return pojo;
     }
 }
