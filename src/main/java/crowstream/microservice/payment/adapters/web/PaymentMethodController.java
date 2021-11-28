1e7d378fec7d0afbbc1df4c4ba5cfa683e6cd4dd
Jorge Aurelio Morales Manrique <jomorales@unal.edu.co>
2021-11-28 15:22:19 -0500

    Modify PUT method

--- src/main/java/crowstream/microservice/payment/adapters/web/PaymentMethodController.java
+++ src/main/java/crowstream/microservice/payment/adapters/web/PaymentMethodController.java
@@ -50,6 +50,9 @@ public class PaymentMethodController {
         if (method == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(String.format("Payment method with id %d does not exist", id));
         }
+        if (!method.getAccountId().equals(paymentMethod.getAccountId())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Account id is not modifiable");
+        }
         PaymentMethod updated = this.servicePort.updatePaymentMethod(paymentMethod);
         return ResponseEntity.status(HttpStatus.OK).body(updated);
     }
