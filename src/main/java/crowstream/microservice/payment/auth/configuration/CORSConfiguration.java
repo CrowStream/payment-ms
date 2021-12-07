a05daaefff00622e532a73f93fd3a9fb641f6d7d
Jorge Aurelio Morales Manrique <jomorales@unal.edu.co>
2021-12-07 00:39:19 -0500

    CORS

---
+++ src/main/java/crowstream/microservice/payment/auth/configuration/CORSConfiguration.java
@@ -0,0 +1,27 @@
+package crowstream.microservice.payment.auth.configuration;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+
+@Configuration
+public class CORSConfiguration {
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin( "http://localhost:8080");
+        config.addAllowedOrigin( "http://104.155.130.109/");
+        config.addAllowedOrigin( "*");
+        config.addAllowedHeader( "*" );
+        config.addAllowedMethod( "*");
+        source.registerCorsConfiguration( "/**", config );
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>( new CorsFilter(  source ) );
+        bean.setOrder( HIGHEST_PRECEDENCE );
+        return bean;
+    }
+}
